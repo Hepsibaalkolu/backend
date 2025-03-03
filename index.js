b9f229c0e8fd833e6ebe33a7eb626851f5aea092
Hema <149656888+Hepsibaalkolu@users.noreply.github.com>
2025-03-03 22:01:56 +0530

    first commit

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect(
-    "mongodb+srv://hema:hema123@cluster0.tgbx8.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0"
-);
+// mongoose.connect(process.env.MONGODBURL);
+mongoose.connect(process.env.MONGODBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err))
 
 app.get("/getUser", (req, res) => {
     UserModel.find({}).then(function(users) {
