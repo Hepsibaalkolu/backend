eea15512ff40f25197ea171491a1efb5bbce6d6e
Hema <149656888+Hepsibaalkolu@users.noreply.github.com>
2025-03-03 21:56:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const UserModel = require('./models/Users');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(
+    "mongodb+srv://hema:hema123@cluster0.tgbx8.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.get("/getUser", (req, res) => {
+    UserModel.find({}).then(function(users) {
+        res.json(users)
+    }).catch(function(err) {
+        res.json(err)
+    })
+})
+
+app.post("/createUser", async (req,res)=>{
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.json(user);
+})
+
+const port = 4800;
+app.listen(port, ()=>{
+    console.log(`server is listening on ${port}`);
+    
+})
+//"http://localhost:5173", 
+//https://website-app-client.vercel.app
+// https://website-app-1whq.vercel.app
+
+// "scripts": {
+//     "test": "echo \"Error: no test specified\" && exit 1",
+//     "start": "nodemon index.js"
+    
+//   },
