7b33da400eefaafa4b07a9632c62a62c6d6b2140
Hema <149656888+Hepsibaalkolu@users.noreply.github.com>
2025-03-06 15:51:05 +0530

    ok

--- index.js
+++ index.js
@@ -24,24 +24,43 @@ app.use(express.json());
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
-app.get("/getUser", (req, res) => {
-    UserModel.find({}).then(function(users) {
-        res.json(users)
-    }).catch(function(err) {
-        res.json(err)
-    })
-})
+// app.get("/getUser", (req, res) => {
+//     UserModel.find({}).then(function(users) {
+//         res.json(users)
+//     }).catch(function(err) {
+//         res.json(err)
+//     })
+// })
+app.get("/getUser", async (req, res) => {
+    try {
+      const users = await UserModel.find({});
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ error: "Failed to fetch users", details: err.message });
+    }
+  });
+  
+  app.post("/createUser", async (req, res) => {
+    try {
+      const user = req.body;
+      const newUser = new UserModel(user);
+      await newUser.save();
+      res.status(201).json(newUser); // Return the saved user with _id
+    } catch (err) {
+      res.status(500).json({ error: "Failed to create user", details: err.message });
+    }
+  });
 
 app.get('/', (req, res) => {
     res.send('Backend is working!');
   });
 
-app.post("/createUser", async (req,res)=>{
-    const user = req.body;
-    const newUser = new UserModel(user);
-    await newUser.save();
-    res.json(user);
-})
+// app.post("/createUser", async (req,res)=>{
+//     const user = req.body;
+//     const newUser = new UserModel(user);
+//     await newUser.save();
+//     res.json(user);
+// })
 
 const port = 4800;
 app.listen(port, ()=>{
